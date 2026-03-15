1f35ef975269e22f4f7f84b413ef0effc806ac92
Kingofath <89654438+Kingofath@users.noreply.github.com>
2026-03-15 13:38:42 -0500

    latest and greatest

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from root_engine.engine import RootEngine
 from root_engine.enums import Faction
+from root_engine.renderer import RootRenderer
 from root_engine.render import TextRenderer, VisualRenderer
 
 engine = RootEngine(seed=7)
+renderer = RootRenderer()
 text_renderer = TextRenderer()
 visual_renderer = VisualRenderer()
 
-print("=== FULL STATE (TEXT) ===")
-print(text_renderer.render(engine.get_state()))
-
-visual_renderer.render_to_file(engine.get_state(), "root_full_state.svg")
-
-print("\n=== MARQUISE OBSERVATION (TEXT) ===")
 marquise_observation = engine.get_observation(Faction.MARQUISE)
-print(text_renderer.render(marquise_observation))
+visual_renderer.render_to_file(marquise_observation, "root_marquise_observation1.svg")
 
-visual_renderer.render_to_file(marquise_observation, "root_marquise_observation.svg")
-print("\nWrote SVGs: root_full_state.svg, root_marquise_observation.svg")
+actions = engine.get_valid_actions()
+engine.apply_action(actions[1])
+
+marquise_observation = engine.get_observation(Faction.MARQUISE)
+visual_renderer.render_to_file(marquise_observation, "root_marquise_observation2.svg")
