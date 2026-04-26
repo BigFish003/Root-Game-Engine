3ebb25e86d5f5739a442ab822cec935aef71a491
SamT244 <samthehammertenney@gmail.com>
2026-04-26 15:13:31 -0500

    Game testing

--- game_tester.py
+++ game_tester.py
@@ -11,11 +11,11 @@ marquise_observation = engine.get_observation(Faction.MARQUISE)
 render.render_board(marquise_observation, 'game_state.png')
 print(engine.get_valid_actions())
 
-actions = [0,1,1,1,1,0,0,1,1,0,0]
+actions = [0,1,1,1,1,0,0,1,0,0,0,0,0,1]
 
 for i in range(len(actions)):
     engine.apply_action(engine.get_valid_actions()[actions[i]])
-    marquise_observation = engine.get_observation(Faction.MARQUISE)
-    render.render_board(marquise_observation, 'game_state.png')
+    obs = engine.get_observation(Faction.ALLIANCE)
+    render.render_board(obs, 'game_state.png')
     print("Current Faction: ", engine.get_state().turn.current_faction, "Current Phase: ", engine.get_state().turn.phase)
     print(engine.get_valid_actions())
