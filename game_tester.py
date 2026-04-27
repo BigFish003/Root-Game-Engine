8be39fb1aaa3a1f6075870e0784023f5cae3af7d
SamT244 <samthehammertenney@gmail.com>
2026-04-26 21:26:15 -0500

    Game testing

--- game_tester.py
+++ game_tester.py
@@ -11,7 +11,7 @@ marquise_observation = engine.get_observation(Faction.MARQUISE)
 render.render_board(marquise_observation, 'game_state.png')
 print(engine.get_valid_actions())
 
-actions = [0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,1,-1,0,0,0,2,0,0,5,0,0,0,0,0,1,1,0,1,0,2]
+actions = [0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,1,-1,0,0,0,2,0,0,5,0,0,0,0,0]
 r = True
 
 for i in range(len(actions)):
@@ -26,3 +26,54 @@ for i in range(len(actions)):
         engine._state.alliance.supporters.append(1)
         engine._state.alliance.supporters.append(1)
     print(engine.get_valid_actions())
+
+def exec_marq_minmax():
+
+    start_state = engine.get_observation(Faction.ALLIANCE)
+    start_faction = start_state.current_faction
+
+
+    end_states = []
+
+    def search(current_engine, depth=0, max_depth=50):
+        state = current_engine.get_observation(Faction.ALLIANCE)
+        current_player = state.current_faction
+        actions = current_engine.get_valid_actions()
+
+        # Stop if the turn moved to another faction/player
+        if current_player != start_faction:
+            end_states.append(state)
+            return
+
+        # Safety stop so recursion does not run forever
+        if depth >= max_depth:
+            end_states.append(state)
+            return
+
+        # If there are no more actions, this is an end state
+        if not actions:
+            end_states.append(state)
+            return
+
+        # Try every valid action from this state
+        for action in actions:
+            next_engine = current_engine.clone()
+            next_engine.apply_action(action)
+            search(next_engine, depth + 1, max_depth)
+
+    search(engine)
+
+    print(f"Found {len(end_states)} possible end states")
+
+    # Render the first end state just to check it worked
+    if end_states:
+        render.render_board(end_states[0], "game_state.png")
+    else:
+        render.render_board(start_state, "game_state.png")
+
+    return end_states
+
+
+exec_marq_minmax()
+
+
